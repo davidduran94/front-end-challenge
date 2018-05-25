e61c95c128659597098953980082b287ec0b5d53
David Durán <davidduran94@gmail.com>
2018-05-24 23:02:06 -0500

    SideNav of markets view

--- bitsotrader/src/Components/markets/markets.js
+++ bitsotrader/src/Components/markets/markets.js
@@ -15,16 +15,39 @@ class Markets extends Component {
 		event.target.setAttribute('stroke-width', '0.1');
 	}
 
+
+	openNav() {
+    	document.getElementById("mySidenav").style.width = "250px";
+    	document.getElementById("marketItemC").style.display = "block";
+	}
+
+	closeNav() {
+	    document.getElementById("mySidenav").style.width = "40px";
+	    document.getElementById("marketItemC").style.display = "none";
+
+	}
+
 	render(){
 		return (
+			<div className="col2">
+			     <div id="mySidenav" className="sidenav">
 
-			 <div className="col2 merc">
-		      <div className="colVertical">
-		      	<a href="#" className="selectIconContainer verticalI"> <img className="arrowDown2" src="/dist/Assets/Images/SVG/icon_dropdown.svg" /> </a>
-		      	<span className="vertical"> MERCADOS </span> 
-		      </div>
-		     </div>
+			     <div className="col2 merc">
+			      <div className="colVertical">
+			      	<a href="javascript:void(0)"  onMouseOver={this.openNav} onClick={this.closeNav} className="selectIconContainer right verticalI"> <img className="arrowDown2" src="/dist/Assets/Images/SVG/icon_dropdown.svg" /> </a>
+			      	<span className="vertical"> MERCADOS </span> 
+			      </div>
+			     </div>
 
+			     <div id="marketItemC">
+				  <div className="topnav"></div>
+				  <div className="topnav"></div>
+				  <div className="topnav"></div>
+				  <div className="topnav"></div>
+				  <div className="topnav"></div>
+				  </div>
+				</div>
+			</div>
 			)
 	}
 }
