3eaae4d680d4945bc57de234a9f17aba030b94fe
David Durán <davidduran94@gmail.com>
2018-05-28 00:03:26 -0500

    Fix on bitso API integration

--- bitsotrader/src/Components/workspace/subheader.js
+++ bitsotrader/src/Components/workspace/subheader.js
@@ -17,34 +17,34 @@ class SubHeader extends Component {
 
 			<div id="subheader" className="subheader"> 
 				      <div className="col5 dataSH" onClick={((e) => this.showCurrencies(e))}> 
-				      	<p className="greenLetter letterBig1" > {this.props.currencyPair} </p> 
+				      	<p className="greenLetter letterBig1" > {this.props.currencyPair} </p>  
 				      	<a href="#" className="arrowContainer1"> <img className="arrowDown" src="/dist/Assets/Images/SVG/icon_dropdown.svg" /> </a>
 				      </div>
 				      <div id="currencyOptions" onMouseLeave={((e) => this.hideCurrencies(e))}>
 				      	<ul className="dropdown-options" >
-				      		<li className="">BTC/MXN</li>
-				      		<li className="">ETH/MXN</li>
-				      		<li className="">XRP/MXN</li>
-				      		<li className="">LTC/MXN</li>
-				      		<li className="">BCH/MXN</li>
+				      		<li> <a className="changeCurrLink" href="/btc_mxn/1month"> BTC/MXN </a> </li>
+				      		<li className=""><a className="changeCurrLink" href="/eth_mxn/1month"> ETH/MXN </a></li>
+				      		<li className=""><a className="changeCurrLink" href="/xrp_mxn/1month"> XRP/MXN </a></li>
+				      		<li className=""><a className="changeCurrLink" href="/ltc_mxn/1month"> LTC/MXN </a></li>
+				      		<li className=""><a className="changeCurrLink" href="/bch_mxn/1month"> BCH/MXN </a></li>				      		
 				      	</ul>
 				      </div>
 
 				      <div className="col16 dataSH"> 
 				      	<p className="darkLetter">Volumen 24 hrs.</p>
-				      	<p className="lightLetter">170.5405818 BTC</p> 
+				      	<p className="lightLetter">{"170.5405818 " + this.props.currencyPair.split("_")[0]}</p> 
 				      </div>
 				      <div className="col16 dataSH"> 
 				        <p className="darkLetter">Max.</p>
-				      	<p className="lightLetter">304,934.23 MXN</p>
+				      	<p className="lightLetter">{"304,934.23 " + this.props.currencyPair.split("_")[1]}</p>
 				      </div>
 				      <div className="col16 dataSH"> 
 				      	<p className="darkLetter">Min.</p>
-				      	<p className="lightLetter">274,934.23 MXN</p>
+				      	<p className="lightLetter">{"274,934.23 " + this.props.currencyPair.split("_")[1] }</p>
 				      </div>
 				      <div className="col16 dataSH"> 
 				      	<p className="darkLetter">Variación.</p>
-				      	<p className="lightLetter">+4,061.68 MXN</p>
+				      	<p className="lightLetter">{"+4,061.68 " + " (1.4%)" }</p>
 				      </div>
 			</div>
 
